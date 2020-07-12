a3fc51ff4bdb3cebd725b8a72e4c546b5d7d43cb
Jose RF Junior <web2ajax@gmail.com>
2020-07-12 14:11:28 -0400

    update componnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {CountryService, CountryServiceCreate, CountryServiceDelete,
         CountryServiceGetAll, CountryServiceGetById, CountryServiceUpdate } from './generated/country_pb_service';
 import {EmptyRequest, CountriesReply, CountryCreateRequest, CountrySearchRequest } from './generated/country_pb';
 import { CountryModel } from './models/countryModel';
+import {host} from '@angular-devkit/build-angular/src/test-utils';
 
 
 @Component({
@@ -15,37 +16,22 @@ export class AppComponent implements OnInit {
 
   public title = 'gRPC';
   public countries: CountryModel[] = [];
+  public host  = 'https://demogrpcweblinux.azurewebsites.net'; /* host: 'https://demogrpcweblinux.azurewebsites.net',*/
 
   public ngOnInit() {
+    this.getCoutryFindAll();
+    this.getCoutryFindName();
+    this.getCoutryFindId();
+    this.getCoutryCreate();
+    this.getCoutryUpdate();
+    this.getCoutryDelete();
+  }
 
+  getCoutryFindAll() {
     const getCountryServiceGetAll  = new EmptyRequest();
-    const getCountryServiceGetById = new EmptyRequest();
-    const getCountryServiceCreate  = new EmptyRequest();
-    const getCountryServiceUpdate  = new EmptyRequest();
-    const getCountryServiceDelete  = new EmptyRequest();
-
     grpc.unary(CountryService.GetAll, {
       request: getCountryServiceGetAll,
-     /* host: 'https://demogrpcweblinux.azurewebsites.net',*/
-      host:'https://grpcwebdemo.azurewebsites.net',
-      onEnd: res => {
-        const { status, message} = res;
-        if (status === grpc.Code.OK && message) {
-        const result = message.toObject() as CountriesReply.AsObject;
-        this.countries = result.countriesList.map(country =>
-          ({
-            id: country.id,
-            name: country.name,
-            description: country.description
-          }) as CountryModel);
-        }
-      }
-    });
-
-    grpc.unary(CountryService.GetById, {
-      request: getCountryServiceGetById,
-      /*https://grpcwebdemo.azurewebsites.net (Windows App Service)*/
-      host: 'https://demogrpcweblinux.azurewebsites.net',
+      host: this.host,
       onEnd: res => {
         const { status, message} = res;
         if (status === grpc.Code.OK && message) {
@@ -59,8 +45,107 @@ export class AppComponent implements OnInit {
         }
       }
     });
+    }
 
-
-
+  getCoutryFindName() {
+    const getCountryServiceGetByName  = new EmptyRequest();
+    grpc.unary(CountryService.GetAll, {
+      request: getCountryServiceGetByName,
+      host: this.host,
+      onEnd: res => {
+        const { status, message} = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              id: country.id,
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
   }
+
+  getCoutryCreate() {
+    const getCountryServiceCreate  = new EmptyRequest();
+    grpc.unary(CountryService.GetById, {
+      request: getCountryServiceCreate,
+      host: this.host,
+      onEnd: res => {
+        const { status, message} = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              id: country.id,
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+  }
+
+  getCoutryFindId() {
+    const getCountryServiceGetById  = new EmptyRequest();
+    grpc.unary(CountryService.GetById, {
+      request: getCountryServiceGetById,
+      host: this.host,
+      onEnd: res => {
+        const { status, message} = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              id: country.id,
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+  }
+
+  getCoutryUpdate() {
+    const getCountryServiceUpdate  = new EmptyRequest();
+    grpc.unary(CountryService.GetById, {
+      request: getCountryServiceUpdate,
+      host: this.host,
+      onEnd: res => {
+        const { status, message} = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              id: country.id,
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+  }
+
+  getCoutryDelete() {
+    const getCountryServiceDelete  = new EmptyRequest();
+    grpc.unary(CountryService.GetById, {
+      request: getCountryServiceDelete,
+      host: this.host,
+      onEnd: res => {
+        const { status, message} = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              id: country.id,
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+  }
+
 }
+
